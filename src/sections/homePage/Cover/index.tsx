3ce5aed6f8a8c67d4f12063fa084b7ff1b03c195
AnotherDar <rosariodaren9@gmail.com>
2022-04-23 08:46:22 -0400

    order

--- src/sections/homePage/Cover/index.tsx
+++ src/sections/homePage/Cover/index.tsx
@@ -16,12 +16,12 @@ export const Hero = () => {
         <header className='px-4 flex flex-col bg-white-soft lg:pb-12'>
             <Nav />
             <div className='flex-grow container mx-auto flex flex-col lg:flex-row w-full h-[65%]'>
-                <div className='lg:w-[40%] lg:p-12 flex flex-col justify-center h-full'>
+                <div className='lg:w-[40%] lg:p-12 flex flex-col justify-center h-full order-2 lg:order-1'>
                     <h1 className='text-4xl lg:text-5xl lg:leading-[3.5rem] font-bold mx-auto text-gray-primary mb-6'>The Sales Completion Platform</h1>
                     <p className='text-gray-secondary mb-6 w-full lg:w-[80%]'>An easier, faster way to complete the entire sales process with mobile-first eSignatures, document collection, ID verification, e-forms, payment and much more.</p>
                     <Button className='lg:text-lg'>See what we’re building - Book Demo!</Button>
                 </div>
-                <div className='lg:w-[60%] flex items-center justify-center'>
+                <div className='lg:w-[60%] flex items-center justify-center order-1 lg:order-2'>
                     <Image
                         src={Banner}
                         alt='Banner'
