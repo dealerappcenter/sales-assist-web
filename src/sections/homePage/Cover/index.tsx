7ab2cc0e53cd73e6a94efd1045367724b7481884
AnotherDar <rosariodaren9@gmail.com>
2022-05-04 18:34:09 -0400

    changing hero text

--- src/sections/homePage/Cover/index.tsx
+++ src/sections/homePage/Cover/index.tsx
@@ -21,7 +21,7 @@ export const Hero = () => {
             <div className='flex-grow container mx-auto flex flex-col lg:flex-row w-full h-[65%] gap-6'>
                 <div className='lg:w-[40%] lg:p-12 flex flex-col justify-center h-full order-2 lg:order-1 items-center lg:items-start'>
                     <h1 className='text-4xl lg:text-5xl lg:leading-[3.5rem] font-bold mx-auto text-gray-primary mb-6'>The Sales Completion Platform</h1>
-                    <p className='text-gray-secondary mb-6 w-full lg:w-[80%]'>An easier, faster way to complete the entire sales process with mobile-first eSignatures, document collection, ID verification, e-forms, payment and much more.</p>
+                    <p className='text-gray-secondary mb-6 w-full lg:w-[80%]'>An easier, faster way to complete the sales process with mobile-first e-Signatures, document collection, ID verification, e-Forms, payment and much more.</p>
                     <Button className='lg:text-lg'>See what we’re building - Book Demo!</Button>
                 </div>
                 <div className='lg:w-[50%] md:px-16 px-12 flex items-center justify-center order-1 lg:order-2'>
