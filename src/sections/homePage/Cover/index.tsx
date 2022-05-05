e8e49233a49fbc05183cadf9c35f1f32ce399ffc
AnotherDar <rosariodaren9@gmail.com>
2022-05-04 21:46:54 -0400

    coming soon

--- src/sections/homePage/Cover/index.tsx
+++ src/sections/homePage/Cover/index.tsx
@@ -22,7 +22,7 @@ export const Hero = () => {
                 <div className='lg:w-[40%] lg:p-12 flex flex-col justify-center h-full order-2 lg:order-1 items-center lg:items-start'>
                     <h1 className='text-4xl lg:text-5xl lg:leading-[3.5rem] font-bold mx-auto text-gray-primary mb-6'>The Sales Completion Platform</h1>
                     <p className='text-gray-secondary mb-6 w-full lg:w-[80%]'>An easier, faster way to complete the sales process with mobile-first e-Signatures, document collection, ID verification, e-Forms, payment and much more.</p>
-                    <Button className='lg:text-lg'>See what we’re building - Book Demo!</Button>
+                    {/* <Button className='lg:text-lg'>See what we’re building - Book Demo!</Button> */}
                 </div>
                 <div className='lg:w-[50%] md:px-16 px-12 flex items-center justify-center order-1 lg:order-2'>
                     <div className='w-[90%]'>
@@ -32,13 +32,13 @@ export const Hero = () => {
                 </div>
             </div>
             <div className='h-[35%] container mx-auto flex flex-col items-center py-12'>
-                <h2 className='mb-[3rem]'>Trusted by</h2>
+                <h2 className='mb-[2rem] text-3xl text-center'>Full site coming soon. Stay tuned!</h2>
                 <div className='flex justify-evenly w-full items-center flex-wrap'>
-                    {trusted.map((t) => {
+                    {/* {trusted.map((t) => {
                         return <div key={Date.now() * Math.random()} className="p-4">
                             <Image className='m-12' alt={'trusted'} src={t}  />
                         </div>
-                    })}
+                    })} */}
                 </div>
             </div>
         </header>
