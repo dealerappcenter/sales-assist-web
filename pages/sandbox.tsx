7c4c706e85e426bf78108fcec23fc3088a4cfcba
AnotherDar <rosariodaren9@gmail.com>
2022-05-19 20:46:07 -0400

    animation

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,9 +1,10 @@
 import { AnimatedBackGround } from "@src/animations/AnimatedBackground"
+import { AnimatedWorldMap } from "@src/animations/WorldMap"
 
 const Sandbox = () => {
   return (
-    <div className="h-screen w-full flex gap-12 px-12">
-      <AnimatedBackGround />
+    <div className="bg-gray-primary w-full flex gap-12 px-12">
+      <AnimatedWorldMap />
     </div>
   )
 }
--- src/animations/WorldMap/index.tsx
+++ src/animations/WorldMap/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const WorldMap = () => {
+export const AnimatedWorldMap = () => {
   return (
-    <div>WorldMap</div>
+    <div>
+
+    </div>
   )
 }
