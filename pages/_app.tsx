6b3a8c052f72c15b164bb82211d991266fd7c9a4
AnotherDar <rosariodaren9@gmail.com>
2022-06-13 16:55:27 -0400

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'swiper/css';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script src='https://www.googletagmanager.com/gtag/js?id=G-YXW1Y5B8CM' strategy="afterInteractive"></Script>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-YXW1Y5B8CM');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
