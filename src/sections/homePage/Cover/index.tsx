fa8aed47a2fe46a58b788a1fb1f72f56acdeb321
AnotherDar <rosariodaren9@gmail.com>
2022-08-09 15:48:22 -0400

    auto animate

--- src/sections/homePage/Cover/index.tsx
+++ src/sections/homePage/Cover/index.tsx
@@ -3,19 +3,22 @@ import Image from 'next/image';
 import { partners, HeroImage } from '@src/assets';
 import { calendlyLink } from '@src/utils/routes';
 import { Section } from '@components/section';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef} from 'react';
 
 interface HeroProps {
     heroData: Hero
 }
 export const Hero: React.FC<HeroProps> = ({ heroData }) => {
     const [currentModule, setCurrentModule] = useState(0);
+    const divRef = useRef<HTMLDivElement | null>();
 
-    useEffect(() => {
+    useEffect(() => { 
         const time = setTimeout(() => {
             if (currentModule === (heroData.modules.length - 1)) {
                 setCurrentModule(0)
+                divRef.current?.scrollTo(0, 0)
             } else {
+                divRef.current?.scrollTo(0, (divRef.current?.children[currentModule] as any).offsetTop)
                 setCurrentModule(currentModule + 1)
             }
             clearTimeout(time)
@@ -26,21 +29,24 @@ export const Hero: React.FC<HeroProps> = ({ heroData }) => {
         }
     }, [currentModule, heroData.modules]);
 
+
     return (
         <header className='flex flex-col bg-white-soft h-full lg:px-12 pb-12'>
             <Nav />
             <Section className='container mx-auto'>
                 <div className='flex items-center justify-center flex-col gap-6'>
                     <div className='flex flex-col items-center gap-2 py-6'>
-                        <h1 className='text-3xl md:text-6xl'>{heroData.hero.upper_message}</h1>
-                        <h1 className='text-orange-normal text-3xl md:text-6xl'>[ {heroData.modules[currentModule]} ]</h1>
-                        <h1 className='text-3xl md:text-6xl'>{heroData.hero.lower_message}</h1>
+                        <h1 className='text-4xl md:text-6xl lg:text-7xl'>{heroData.hero.upper_message}</h1>
+                        <div ref={el => divRef.current = el} className='text-center overflow-hidden overflow-y-scroll gap-6 flex flex-col h-[40px] md:h-[65px] lg:h-[80px] w-full slices'>
+                            {heroData.modules.map((m, i) => <h1 key={m} className='text-orange-normal text-4xl md:text-6xl lg:text-7xl slice'>{m}</h1>)}
+                        </div>
+                        <h1 className='text-4xl md:text-6xl lg:text-7xl'>{heroData.hero.lower_message}</h1>
                     </div>
 
                     <div className='max-w-4xl'>
-                        <p className='text-gray-secondary font-light'>
+                        <p className='text-gray-secondary text-justify'>
                             {heroData.desc.message}{' '}
-                            <span className='font-bold border-b-2 border-orange-normal'>{heroData.desc.highLight}</span>{' '}
+                            <span className='font-bold underline underline-offset-2 decoration-orange-normal'>{heroData.desc.highLight}</span>{' '}
                             {heroData.desc.message_end}
                         </p>
                     </div>
