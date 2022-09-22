8b7d9dc9a7bc25007b1bd43589d6ba1bfdb86df7
Rogelio Morey <sparohawk@gmail.com>
2022-09-22 08:22:42 -0400

    fix: set nav max width at 1200px

--- src/sections/home-page/Cover/index.tsx
+++ src/sections/home-page/Cover/index.tsx
@@ -29,7 +29,7 @@ export const Hero: React.FC<HeroProps> = ({ heroData }) => {
 
   return (
     <header className='flex flex-col bg-white-soft h-full mx-auto pb-12 max-w-4xl'>
-      <Section className='container mx-auto'>
+      <Section className='px-0'>
         <div className='flex justify-center flex-col gap-6'>
           <div className='flex flex-col pb-6'>
             <h1 className='text-4xl md:text-6xl lg:text-7xl'>{heroData.hero.upper_message}</h1>
