965c970c18e6f4be8bef2b172313561f9aaf4a9e
Rogelio Morey <sparohawk@gmail.com>
2023-11-08 19:08:17 -0400

    feat: add new video section

---
+++ src/components/video/index.tsx
@@ -0,0 +1,23 @@
+import { Section } from '@components/section';
+
+export default function Video() {
+  return (
+    <section className='section !pb-0'>
+      <Section>
+        <div className="aspect-video w-full max-w-screen-xl">
+          <iframe
+            src="https://www.youtube.com/embed/0nX_XCLDhsE?list=PLOIszb58G-I9QtzrZzt0miUjuihqPeCIS&rel=0&hd=1"
+            width="100%"
+            height="100%"
+            title="SalesAssist Get Docs Demo"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            style={{
+              border: 'none'
+            }}
+          />
+        </div>
+      </Section>
+    </section>
+  );
+}
