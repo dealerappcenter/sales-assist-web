c588652a6cdbec1360d56fbabdbdb31e42fca41a
Bryan Astacio <brya0196@gmail.com>
2025-03-10 17:16:36 -0400

    feat: include the QR Tenafly Whatsapp link

--- pages/onboarding/[slug].tsx
+++ pages/onboarding/[slug].tsx
@@ -5,7 +5,7 @@ import { Nav, Button } from '@components/index';
 import Header from '@components/header';
 import { Footer } from '@components/Footer';
 import DoubleColumn from '@components/double-column';
-import { Griffin, TomGibbsChevy, SocialMediaImages, Store, ManlyImages } from '@src/assets';
+import { Griffin, TomGibbsChevy, SocialMediaImages, Store, ManlyImages, TenaflyImages } from '@src/assets';
 import { Onboarding as OB } from '@utils/routes';
 import { useMemo } from 'react';
 
@@ -53,7 +53,7 @@ const crmData: Record<string, Props> = {
     title: 'Honda of Tenafly',
     tempPassword: 'tenafly2024',
     crm: 'Vinsolutions',
-    // images: TomGibbsChevy,
+    images: TenaflyImages,
   },
 };
 
