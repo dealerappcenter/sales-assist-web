f27e0d271f62c1ef69b4be6c08963f601f46743f
Bryan Astacio <brya0196@gmail.com>
2025-04-28 10:27:00 -0400

    feat: included security policy content

--- pages/security-policy.tsx
+++ pages/security-policy.tsx
@@ -1,7 +1,10 @@
 import { Footer, Nav } from "@src/components";
 import Head from "next/head";
 
+import policy from '@src/mocks/security/policy.json'
+
 export default function SecurityPolicy() {
+
   return(
     <>
       <Head>
@@ -14,7 +17,43 @@ export default function SecurityPolicy() {
         <Nav />
       </header>
 
+      <section id="terms-of-service">
+        <div className="container mx-auto py-12 flex flex-col gap-6 lg:px-12 md:px-6 px-4 text-gray-primary">
+          <div className="flex flex-col gap-6">
+            <div className="text-center">
+              <h1 className="font-bold text-4xl">{policy.title}</h1>
+            </div>
+            {/* <p className="text-base">{policy.effectiveDate}</p> */}
+            {policy.introduction.map((paragraph, index) => <p key={`introduction-${index}`} className="text-base">{paragraph}</p>)}
+          </div>
 
+          {
+            policy.data.map(function (item, i) {
+              const key = item.title.toLowerCase().split(' ').join('-')
+              return (
+                <div className="flex flex-col gap-6" key={key}>
+                  <h1 className="font-bold text-xl">{i + 1}. {item.title}</h1>
+                  {item.subtext?.map(function (text, index) {
+                    return <p className="text-base" key={`${key}subtext-${index}`}>{text}</p>
+                  })}
+
+                  <ul className="list-disc pl-6 md:pl-12 flex flex-col gap-4">
+                    {item.text.map(function (text, index) {
+                      return <li key={`${key}-text-${index}`}>{text}</li>
+                    })}
+                  </ul>
+                  
+                </div>
+              )
+            })
+          }
+          
+          <div>
+            <p className="text-base font-semibold">{policy.footer.title}</p>
+            <p className="text-base">{policy.footer.text} <a href={`mailto:${policy.footer.email}`} className="text-orange-600">{policy.footer.email}</a> </p>
+          </div>
+        </div>
+      </section>
       <Footer kind="dark" />
     </>
   )
